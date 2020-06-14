c145c2cf841cd9e7916da729dd31cb1e40d97408
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-14 07:46:04 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -7,14 +7,17 @@ export default function manageRestaurants(state = {restaurants: [], reviews: []}
   case 'ADD_RESTAURANT':
     const restaurant = {id: cuidFn(), text: action.text}
     return {...state, restaurants: [...state.restaurants, restaurant]}
+    
   case 'DELETE_RESTAURANT':
     const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
     return {restaurants}
+    
   case 'ADD_REVIEW':
     const review = {restaurantId: cuidFn, id: Math.random(), text: action.text}
     return {...state, reviews: [...state.reviews, review]}
     const review = {restaurantId: cuidFn, id: Math.random() text: action.text}
     return {...state, reviews: [...state.reviews: review]}
+    
   case 'DELETE_REVIEW':
     const reviews = state.reviews.filter(review => review.id !== action.id)
     return {reviews}
