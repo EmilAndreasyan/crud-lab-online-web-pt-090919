fc077a3ec3f51e30d0f8e3592dfa34f575b279f5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-14 08:09:23 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,7 +4,7 @@ import Review from './Review';
 class Reviews extends Component {
   render() {
     const {reviews, restaurantId, deleteReview} = this.props
-    const assiciatedReviews = reviews.filter(review => )
+    const assiciatedReviews = reviews.filter(review => review.restaurantId === restaurantId)
     const reviewList = reviews.map(review => <Review key={review.restaurantId} {...review} deleteReview={this.props.deleteReview}/>)
     return (
       <ul>
