0fe716b0e48b2f6a1e48957d9481042585ab46e7
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-14 08:07:07 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -22,7 +22,7 @@ class ReviewInput extends Component {
        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
-            value={this.state.bandName}
+            value={this.state.text}
             onChange={this.handleChange} />
           <input type="submit" />
         </form>
