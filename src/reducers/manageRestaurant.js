64b61c1b15254cd31108753dd40cb43b2d47ce71
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-14 07:45:39 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,7 +5,7 @@ export const cuidFn = cuid;
 export default function manageRestaurants(state = {restaurants: [], reviews: []}, action) {
   switch(action.type) {
   case 'ADD_RESTAURANT':
-    const restaurant = {id: cuidFn, text: action.text}
+    const restaurant = {id: cuidFn(), text: action.text}
     return {...state, restaurants: [...state.restaurants, restaurant]}
   case 'DELETE_RESTAURANT':
     const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
